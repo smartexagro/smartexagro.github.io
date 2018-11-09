66f5f7a9e35eea52f51f2c12c6063509fa094340
Hans Bonini <hans@ysolutions.com.br>
2018-11-09 21:57:34 -0200

    Novo site

--- _posts/2018-11-09-welcome-to-jekyll.markdown
+++ _posts/2018-11-09-welcome-to-jekyll.markdown
@@ -1,5 +1,5 @@
 ---
-layout: post
+layout: default
 title:  "Welcome to Jekyll!"
 date:   2018-11-09 15:01:19 -0200
 categories: jekyll update
---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+$( document )
+  .ready( function ( $ ) {
+
+    function deferVideo() {
+      $( "video source" )
+        .each( function () {
+          var sourceFile = $( this )
+            .attr( "data-src" );
+          $( this )
+            .attr( "src", sourceFile );
+          var video = this.parentElement;
+          video.load();
+        } );
+    }
+
+    // Navbar fixa após rolagem
+    $( window )
+      .on( 'scroll', function () {
+        if ( $( window )
+          .scrollTop() >= $( window )
+          .height() + 1 ) {
+          $( '.navbar' )
+            .addClass( 'sticky-top' );
+          $( '.scroll-top' )
+            .fadeIn();
+        } else {
+          $( '.navbar' )
+            .removeClass( 'sticky-top' );
+          $( '.scroll-top' )
+            .fadeOut();
+        }
+      } );
+
+    // Botão Conhecer
+    $( '#conhecer' )
+      .on( 'click', function () {
+        //
+      } );
+
+    // Defer para carregamento posterior do video
+    deferVideo();
+
+  } )
